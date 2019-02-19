6966ab9183ff63c154d0139c77ce4fb45b33ce14
abstract-animation-9914 <vm930@hotmail.com>
2019-02-19 20:36:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,12 @@
+require 'pry'
+
 def game_hash
   {
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: [
-        {
-          player_name: "Alan Anderson",
+      players: {
+        "Alan Anderson" => {
           number: 0,
           shoe: 16,
           points: 22,
@@ -14,8 +15,8 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 1
-        }, {
-          player_name: "Reggie Evans",
+        },
+        "Reggie Evans" => {
           number: 30,
           shoe: 14,
           points: 12,
@@ -24,8 +25,8 @@ def game_hash
           steals: 12,
           blocks: 12,
           slam_dunks: 7
-        }, {
-          player_name: "Brook Lopez",
+        },
+        "Brook Lopez" => {
           number: 11,
           shoe: 17,
           points: 17,
@@ -34,8 +35,8 @@ def game_hash
           steals: 3,
           blocks: 1,
           slam_dunks: 15
-        }, {
-          player_name: "Mason Plumlee",
+        },
+        "Mason Plumlee" => {
           number: 1,
           shoe: 19,
           points: 26,
@@ -44,8 +45,8 @@ def game_hash
           steals: 3,
           blocks: 8,
           slam_dunks: 5
-        }, {
-          player_name: "Jason Terry",
+        },
+        "Jason Terry" => {
           number: 31,
           shoe: 15,
           points: 19,
@@ -55,14 +56,13 @@ def game_hash
           blocks: 11,
           slam_dunks: 1
         }
-      ]
+      }
     },
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turquoise", "Purple"],
-      players: [
-        {
-          player_name: "Jeff Adrien",
+      players: {
+        "Jeff Adrien" => {
           number: 4,
           shoe: 18,
           points: 10,
@@ -71,8 +71,8 @@ def game_hash
           steals: 2,
           blocks: 7,
           slam_dunks: 2
-        }, {
-          player_name: "Bismak Biyombo",
+        },
+        "Bismak Biyombo" => {
           number: 0,
           shoe: 16,
           points: 12,
@@ -81,8 +81,8 @@ def game_hash
           steals: 7,
           blocks: 15,
           slam_dunks: 10
-        }, {
-          player_name: "DeSagna Diop",
+        },
+        "DeSagna Diop" => {
           number: 2,
           shoe: 14,
           points: 24,
@@ -91,8 +91,8 @@ def game_hash
           steals: 4,
           blocks: 5,
           slam_dunks: 5
-        }, {
-          player_name: "Ben Gordon",
+        },
+        "Ben Gordon" => {
           number: 8,
           shoe: 15,
           points: 33,
@@ -101,8 +101,8 @@ def game_hash
           steals: 1,
           blocks: 1,
           slam_dunks: 0
-        }, {
-          player_name: "Brendan Haywood",
+        },
+        "Brendan Haywood" => {
           number: 33,
           shoe: 15,
           points: 6,
@@ -112,70 +112,104 @@ def game_hash
           blocks: 5,
           slam_dunks: 12
         }
-      ]
+      }
     }
   }
 end
 
-def shoe_size(name)
-  player = find_the_player(name)
-  player.fetch(:shoe)
-end
+# Build a method, num_points_scored that takes in an argument of a player's name and returns the number of points scored for that #player.
 
-def num_points_scored(name)
-  player = find_the_player(name)
-  player.fetch(:points)
-end
+def num_points_scored (player_name)
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |player, player_data| 
+      #binding.pry
+       if player == player_name
+         return player_data[:points]
+     end  
+    end 
+  end 
+end 
+
+# Build a method, shoe_size, that takes in an argument of a player's name and returns the shoe size for that player.
+
+def shoe_size(player_name)
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |player, player_data|
+       if player == player_name
+       return player_data[:shoe]
+      end
+    end
+  end 
+end 
+
+# Build a method, team_colors, that takes in an argument of the team name and returns an array of that teams colors.
 
 def team_colors(team_name)
-  team = find_the_team(team_name)
-  team.fetch(:colors)
+  game_hash.each do |team, team_data|
+    # binding.pry
+    if team_data[:team_name] == team_name
+    return team_data[:colors] #as array of that team colors
+  end
+ end
 end
 
-def teams
-  game_hash.values
-end
-
-def find_the_team(team_name)
-  teams.find {|team| team.fetch(:team_name) == team_name}
-end
-
-def big_shoe_rebounds
-  player = player_biggest_shoe_size
-  player.fetch(:rebounds)
-end
-
-def player_biggest_shoe_size
-  players.sort_by {|player| player.fetch(:shoe) }.last
-end
-
-def players
-  home_players = game_hash.fetch(:home).fetch(:players)
-  away_players = game_hash.fetch(:away).fetch(:players)
-  home_players + away_players
-end
+#Build a method, team_names, that operates on the game hash to return an array of the team names.
 
 def team_names
-  teams.map do |team|
-    team[:team_name]
+  game_hash.map do |team, team_data|
+      team_data[:team_name]
+     ##array of team names
+   end
+end 
+
+#player_numbers, takes in an argument of a team name and returns an array of the jersey number's for that team.
+
+def player_numbers (team_name)
+  team_players = []
+  game_hash.each do 
+    |team, team_data|
+    if team_data[:team_name] == team_name
+      team_players = team_data[:players].collect do
+      |player, player_data| player_data[:number]
+    end
+    return team_players
+    end
   end
 end
 
-def player_numbers(team_name)
-  find_the_team(team_name)[:players].map do |player|
-    player[:number]
-  end
-end
+
+#Build a method, player_stats, that takes in an argument of a player's name and returns a hash of that player's stats.
 
 def player_stats(player_name)
-  find_the_player(player_name).reject { |key, value| key == :player_name }
+  game_hash.each do |team, team_data| 
+    team_data[:players].each do 
+      |player, player_stats|
+      # binding.pry
+      if player == player_name
+        return player_stats
+      end
+    end
+  end
 end
 
-def find_the_player(name)
-  players.find {|player| player.fetch(:player_name) == name}
+#   * Build a method, `big_shoe_rebounds`, that will return the number of rebounds associated with the player that has the largest shoe size. Break this one down into steps:
+#   * First, find the player with the largest shoe size
+#   * Then, return that player's number of rebounds
+#   * Remember to think about return values here. Use `binding.pry` to drop into your method and understand what it is returning and why.
+
+def big_shoe_rebounds
+  #find the player with largest shoe size 
+  max_value = 0
+  game_hash.each do |team, team_data|
+    team_data[:players].each do |player,player_stats|
+       if player_stats[:shoe] > max_value
+         max_value = player_stats[:shoe]
+       end
+      return player_stats[:rebounds]
+    end 
+  end
+  #return that players number of rebounds
 end
-
-
 
 
 
